89a2af9a61160470259503099777c4fcf7277e42
Omar Valencia <omarvalencia9415@gmail.com>
2020-09-03 14:33:56 -0500

    create folders MVC

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const server =express();
-const {PORT} = require("../config")
+const {PORT} = require("../config");
+const {HomeRoutes} = require('../routes');
+const { HomeController } = require('../controllers');
+
+server.use(express.static('../public'));
+server.use(express.json());
+
+server.use("/",HomeRoutes);
 
 server.listen(PORT, () =>{
     console.log(`Aplication running on PORT ${PORT}`);
 });
+
